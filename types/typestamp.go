a8e4470850d43a2d882fa9bf96e7d27d24eb66f8
bmf-san <bmf.infomation@gmail.com>
2022-05-29 13:03:27 +0900

    [update] refacored typestamp

--- types/typestamp.go
+++ types/typestamp.go
@@ -4,7 +4,7 @@ package types
 type TypeStamp int
 
 const (
-	// Attendance is a type of Attendance.
+	// Attendance is a type of attendance.
 	Attendance TypeStamp = 11
 	// LeaveWork is a type of leaving work.
 	LeaveWork TypeStamp = 12
@@ -14,17 +14,40 @@ const (
 	Break TypeStamp = 31
 	// BreakBack is a type of break back.
 	BreakBack TypeStamp = 32
+
+	// TextAttendance is a text of attendance.
+	TextAttendance = "出勤"
+	// TextLeaveWork is a text of leaving work.
+	TextLeaveWork = "退勤"
+	// TextGoStraight is a text of going straight.
+	TextGoStraight = "直行"
+	// TextBreak is a text of break.
+	TextBreak = "休憩入"
+	// TextBreakBack is a text of break back.
+	TextBreakBack = "休憩戻"
 )
 
 var stampText = map[TypeStamp]string{
-	Attendance: "出勤",
-	LeaveWork:  "退勤",
-	GoStraight: "直行",
-	Break:      "休憩入",
-	BreakBack:  "休憩戻",
+	Attendance: TextAttendance,
+	LeaveWork:  TextLeaveWork,
+	GoStraight: TextGoStraight,
+	Break:      TextBreak,
+	BreakBack:  TextBreakBack,
+}
+
+var stampNumber = map[string]TypeStamp{
+	TextAttendance: Attendance,
+	TextLeaveWork:  LeaveWork,
+	TextGoStraight: GoStraight,
+	TextBreak:      Break,
+	TextBreakBack:  BreakBack,
 }
 
 // StampText returns a text of stamp.
 func StampText(t TypeStamp) string {
 	return stampText[t]
+}
+
+func StampNumber(s string) TypeStamp {
+	return stampNumber[s]
 }
